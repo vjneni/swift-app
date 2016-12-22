599a7f1c65010c899834c8ffba1d19e02bebc947
Nannapaneni, Vijay X [Levi, Ray & Shoup Inc Contractor for Sprint] <vijay.nannapaneni@sprint.com>
2016-12-22 11:29:25 -0600

    Initial Commit

---
+++ Swift App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift App
+//
+//  Created by Nannapaneni, Vijay X [Levi, Ray & Shoup Inc Contractor for Sprint] on 12/22/16.
+//  Copyright © 2016 Crap App. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
