633176eed9e2e058b0b70e4b4aa639a949522b59
Nannapaneni, Vijay X [Levi, Ray & Shoup Inc Contractor for Sprint] <vijay.nannapaneni@sprint.com>
2016-12-23 14:53:58 -0600

    Swift App - Initial Commit

--- Swift App/ViewController.swift
+++ Swift App/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var theLabel: UILabel!
+    
+    var tapCount = 0
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount+1;
+        
+        if tapCount >= 10{
+            theLabel.text = "Tapped 10 times!!!!"
+        } else{
+            print(tapCount);
+        }
+        
+      /*  theLabel.text = "Hello Xcode!"
+        print("Button Tapped") */
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        self.view.backgroundColor = UIColor.darkGray
     }
 
     override func didReceiveMemoryWarning() {
